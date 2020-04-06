c9d1ed4398bb95b2ae5d0a57f1f11762352a431a
Harish Reddy Maryada <harishreddymaryada@Harishs-MacBook-Pro.local>
2020-04-06 11:45:50 +0530

    modules os,fs,path

---
+++ nms/os_module.js
@@ -0,0 +1,8 @@
+const os = require('os')
+
+
+const totalMemory = os.totalmem()
+const freeMemory = os.freemem()
+
+console.log(`Total Memory :${totalMemory}`)
+console.log(`Free Memory :${freeMemory}`)
